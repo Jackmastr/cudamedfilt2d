267b16357dd27b813f5c0c5c6c0aaa3636615ef4
Jackson <jsipple@berkeley.edu>
2018-07-13 09:09:40 -0700

    initial commit for the package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(name='cudamedfilt2d',
+	version='1.0',
+	description='A replacement for scipy.signal\'s medfilt2d() function using pycdua'
+	url='[REPLACEM_ME]',
+	author='Jackson Sipple, Aaron Parsons'
+	packages=['cudamedfilt2d'],
+	zip_safe=False)
